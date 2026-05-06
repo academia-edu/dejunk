af32f325fe5c2b6f0c17ed31ef1837a4d0dde3bc
Steve Pike <steve@infield.ai>
2026-05-06 11:01:10 -0400

    Remove deprecated mb_chars method
    This method wrapped strings in a proxy class so that you could use
    regular string methods on UTF-8 encoded strings that had multibyte
    characters in them.
    
    For example in ruby 1.8 you couldn't do this:
    >> "Ç".mb_chars.upcase.to_s
    
    This seems to be a historical artifact at this point as ruby 2.4+
    has supported this natively.
    
    There is also a defensive function in mb_chars where if a string is
    not tagged as UTF-8 encoding the method automatically forces it to that
    encoding (without converting it, so if it wasn't actually UTF-8 data you
    could get confusing output). This commit keeps that behavior but maybe
    it should be removed.

--- lib/dejunk.rb
+++ lib/dejunk.rb
@@ -113,7 +113,7 @@ module Dejunk
     string.
       chars.
       zip(string.chars[1..-1]).
-      map { |c1,c2| "#{c1.mb_chars.downcase}#{c2.mb_chars.downcase}" if c1 && c2 }.
+      map { |c1,c2| "#{c1.downcase}#{c2.downcase}" if c1 && c2 }.
       compact.
       map { |bigram| bigram.gsub(/[0-9]/, '0'.freeze) }.
       map { |bigram| bigram.gsub(/[[:space:]]/, ' '.freeze) }
@@ -144,8 +144,12 @@ module Dejunk
   end
 
   def normalize_for_comparison(string)
+    # This mirrors what mb_chars did, assuming that non-UTF-8 encoded strings
+    # are actually UTF-8 in disguise. It's unclear whether this is necessary,
+    # but we left it in to avoid having to figure this out.
+    string = string.dup.force_encoding(Encoding::UTF_8) if string.encoding != Encoding::UTF_8
+
     string.
-      mb_chars.
       unicode_normalize(:nfkd).
       gsub(/\p{Mn}+/, ''.freeze).
       gsub(/[^[:alnum:]]+/, ''.freeze).
